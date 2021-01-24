26670da296dbc1047e4349c3342325a2e61ce640
GYANAM-DEVELOPER <74140267+GYANAM-ui@users.noreply.github.com>
2021-01-24 19:05:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var text="My first console project"
+console.log(text);
+var text_1="About Visual Studio Microsoft Visual Studio is an integrated development environment from Microsoft. It is used to develop computer programs, as well as websites, web apps, web services and mobile apps.";
+console.log(text_1);
+var text_2="About console Alternatively referred to as a computer console, root console, system console, or terminal. Console is a basic computer or monitor and keyboard that is connected to another computer, server, or a mainframe over a network. It is used to maintain or monitor the status of the network or computer.";
+console.log(text_2);
+var first_name="GYANAM ";
+console.log(first_name);
+var last_name="GOGOI";
+console.log(last_name);
+var full_name=first_name.concat(last_name);
+console.log(full_name)
+var length_of_full_name=full_name.length;
+console.log(length_of_full_name);
+var maximum_number=Math.max(20,50);
+console.log(maximum_number);
+var minimum_number=Math.min(1,5);
+console.log(minimum_number);
+var power1=Math.pow(4,2);
+console.log(power1);
